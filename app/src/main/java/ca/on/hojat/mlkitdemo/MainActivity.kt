4cf585fa7613063dbf021fa6b56282479ce111eb
hojat <hojat72elect@gmail.com>
2023-04-05 01:34:08 -0400

    initial commit

--- app/src/main/java/ca/on/hojat/mlkitdemo/MainActivity.kt
+++ app/src/main/java/ca/on/hojat/mlkitdemo/MainActivity.kt
@@ -1,11 +1,38 @@
 package ca.on.hojat.mlkitdemo
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import androidx.appcompat.app.AppCompatActivity
+import ca.on.hojat.mlkitdemo.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private val countryList = arrayListOf(
+        "CameraX live preview vision detectors",
+        "Still image vision detector",
+        "Live camera translator",
+        "Code scanner",
+        "Digital ink recognition"
+
+
+    )
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        // draw the UI
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val adapter = ArrayAdapter(
+            this,
+            androidx.appcompat.R.layout.support_simple_spinner_dropdown_item,
+            countryList
+        )
+        binding.mainLv.adapter = adapter
+
+
     }
 }
