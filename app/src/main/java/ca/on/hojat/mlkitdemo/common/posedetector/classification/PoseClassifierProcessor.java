d4da355dc986b11a72c423134f61d4a89c538c1d
hojat <hojat72elect@gmail.com>
2023-04-14 02:11:02 -0400

    cleaned

--- app/src/main/java/ca/on/hojat/mlkitdemo/common/posedetector/classification/PoseClassifierProcessor.java
+++ app/src/main/java/ca/on/hojat/mlkitdemo/common/posedetector/classification/PoseClassifierProcessor.java
@@ -1,6 +1,22 @@
 package ca.on.hojat.mlkitdemo.common.posedetector.classification;
 
+import android.content.Context;
+import android.media.AudioManager;
+import android.media.ToneGenerator;
+import android.os.Looper;
+import android.util.Log;
+
+import androidx.annotation.WorkerThread;
+
+import com.google.common.base.Preconditions;
 import com.google.mlkit.vision.pose.Pose;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
 
 /**
  * Accepts a stream of {@link Pose} for classification and Rep counting.
@@ -21,43 +37,43 @@ public class PoseClassifierProcessor {
     private final boolean isStreamMode;
 
     private EMASmoothing emaSmoothing;
-    private java.util.List<ca.on.hojat.mlkitdemo.common.posedetector.classification.RepetitionCounter> repCounters;
+    private List<RepetitionCounter> repCounters;
     private PoseClassifier poseClassifier;
     private String lastRepResult;
 
-    @androidx.annotation.WorkerThread
-    public PoseClassifierProcessor(android.content.Context context, boolean isStreamMode) {
-        com.google.common.base.Preconditions.checkState(android.os.Looper.myLooper() != android.os.Looper.getMainLooper());
+    @WorkerThread
+    public PoseClassifierProcessor(Context context, boolean isStreamMode) {
+        Preconditions.checkState(Looper.myLooper() != Looper.getMainLooper());
         this.isStreamMode = isStreamMode;
         if (isStreamMode) {
             emaSmoothing = new EMASmoothing();
-            repCounters = new java.util.ArrayList<>();
+            repCounters = new ArrayList<>();
             lastRepResult = "";
         }
         loadPoseSamples(context);
     }
 
-    private void loadPoseSamples(android.content.Context context) {
-        java.util.List<ca.on.hojat.mlkitdemo.common.posedetector.classification.PoseSample> poseSamples = new java.util.ArrayList<>();
+    private void loadPoseSamples(Context context) {
+        List<PoseSample> poseSamples = new ArrayList<>();
         try {
-            java.io.BufferedReader reader = new java.io.BufferedReader(
-                    new java.io.InputStreamReader(context.getAssets().open(POSE_SAMPLES_FILE)));
+            BufferedReader reader = new BufferedReader(
+                    new InputStreamReader(context.getAssets().open(POSE_SAMPLES_FILE)));
             String csvLine = reader.readLine();
             while (csvLine != null) {
                 // If line is not a valid {@link PoseSample}, we'll get null and skip adding to the list.
-                ca.on.hojat.mlkitdemo.common.posedetector.classification.PoseSample poseSample = ca.on.hojat.mlkitdemo.common.posedetector.classification.PoseSample.getPoseSample(csvLine, ",");
+                PoseSample poseSample = PoseSample.getPoseSample(csvLine, ",");
                 if (poseSample != null) {
                     poseSamples.add(poseSample);
                 }
                 csvLine = reader.readLine();
             }
-        } catch (java.io.IOException e) {
-            android.util.Log.e(TAG, "Error when loading pose samples.\n" + e);
+        } catch (IOException e) {
+            Log.e(TAG, "Error when loading pose samples.\n" + e);
         }
         poseClassifier = new PoseClassifier(poseSamples);
         if (isStreamMode) {
             for (String className : POSE_CLASSES) {
-                repCounters.add(new ca.on.hojat.mlkitdemo.common.posedetector.classification.RepetitionCounter(className));
+                repCounters.add(new RepetitionCounter(className));
             }
         }
     }
@@ -70,11 +86,11 @@ public class PoseClassifierProcessor {
      * 0: PoseClass : X reps
      * 1: PoseClass : [0.0-1.0] confidence
      */
-    @androidx.annotation.WorkerThread
-    public java.util.List<String> getPoseResult(com.google.mlkit.vision.pose.Pose pose) {
-        com.google.common.base.Preconditions.checkState(android.os.Looper.myLooper() != android.os.Looper.getMainLooper());
-        java.util.List<String> result = new java.util.ArrayList<>();
-        ca.on.hojat.mlkitdemo.common.posedetector.classification.ClassificationResult classification = poseClassifier.classify(pose);
+    @WorkerThread
+    public List<String> getPoseResult(Pose pose) {
+        Preconditions.checkState(Looper.myLooper() != Looper.getMainLooper());
+        List<String> result = new ArrayList<>();
+        ClassificationResult classification = poseClassifier.classify(pose);
 
         // Update {@link RepetitionCounter}s if {@code isStreamMode}.
         if (isStreamMode) {
@@ -87,15 +103,15 @@ public class PoseClassifierProcessor {
                 return result;
             }
 
-            for (ca.on.hojat.mlkitdemo.common.posedetector.classification.RepetitionCounter repCounter : repCounters) {
+            for (RepetitionCounter repCounter : repCounters) {
                 int repsBefore = repCounter.getNumRepeats();
                 int repsAfter = repCounter.addClassificationResult(classification);
                 if (repsAfter > repsBefore) {
                     // Play a fun beep when rep counter updates.
-                    android.media.ToneGenerator tg = new android.media.ToneGenerator(android.media.AudioManager.STREAM_NOTIFICATION, 100);
-                    tg.startTone(android.media.ToneGenerator.TONE_PROP_BEEP);
+                    ToneGenerator tg = new ToneGenerator(AudioManager.STREAM_NOTIFICATION, 100);
+                    tg.startTone(ToneGenerator.TONE_PROP_BEEP);
                     lastRepResult = String.format(
-                            java.util.Locale.US, "%s : %d reps", repCounter.getClassName(), repsAfter);
+                            Locale.US, "%s : %d reps", repCounter.getClassName(), repsAfter);
                     break;
                 }
             }
@@ -106,7 +122,7 @@ public class PoseClassifierProcessor {
         if (!pose.getAllPoseLandmarks().isEmpty()) {
             String maxConfidenceClass = classification.getMaxConfidenceClass();
             String maxConfidenceClassResult = String.format(
-                    java.util.Locale.US,
+                    Locale.US,
                     "%s : %.2f confidence",
                     maxConfidenceClass,
                     classification.getClassConfidence(maxConfidenceClass)
