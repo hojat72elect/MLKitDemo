5eef45a992d6d11552b953d37d2d60c2c587c084
hojat <hojat72elect@gmail.com>
2023-04-25 15:01:04 -0400

    ByteBuffer extension fun

---
+++ app/src/main/java/ca/on/hojat/mlkitdemo/extensions/ByteBuffer.kt
@@ -0,0 +1,38 @@
+package ca.on.hojat.mlkitdemo.extensions
+
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.graphics.ImageFormat
+import android.graphics.Rect
+import android.graphics.YuvImage
+import android.util.Log
+import ca.on.hojat.mlkitdemo.common.FrameMetadata
+import java.io.ByteArrayOutputStream
+import java.nio.ByteBuffer
+
+/**
+ * Converts a NV21 format [ByteBuffer] to a [Bitmap].
+ */
+fun ByteBuffer.getBitmap(metadata: FrameMetadata): Bitmap? {
+
+    rewind()
+    val imageInBuffer = ByteArray(limit())
+    get(imageInBuffer, 0, imageInBuffer.size)
+    try {
+        val image = YuvImage(imageInBuffer, ImageFormat.NV21, metadata.width, metadata.height, null)
+        val stream = ByteArrayOutputStream()
+        image.compressToJpeg(Rect(0, 0, metadata.width, metadata.height), 80, stream)
+
+        val bmp = BitmapFactory.decodeByteArray(stream.toByteArray(), 0, stream.size())
+
+        stream.close()
+        return bmp.rotateBitmap(
+            rotationDegrees = metadata.rotation,
+            flipX = false,
+            flipY = false
+        )
+    } catch (e: Exception) {
+        Log.e("VisionProcessorBase", e.message, e)
+    }
+    return null
+}
