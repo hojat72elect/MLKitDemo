a0a142dcd90db6522cd1fa788e7772bfcb8be865
hojat <hojat72elect@gmail.com>
2023-04-25 13:44:20 -0400

    Plane extension funs

--- app/src/main/java/ca/on/hojat/mlkitdemo/extensions/Plane.kt
+++ app/src/main/java/ca/on/hojat/mlkitdemo/extensions/Plane.kt
@@ -43,3 +43,34 @@ fun Plane.unpack(
     }
 }
 
+
+/**
+ * Is applied to an Array of [Plane]s and checks if the
+ * UV plane buffers of a YUV_420_888 image are in the NV21
+ * format.
+ */
+fun Array<Plane>.areNV21(width: Int, height: Int): Boolean {
+
+    val imageSize = width * height
+    val uBuffer = get(1).buffer
+    val vBuffer = get(2).buffer
+
+    // Backup buffer properties.
+    val vBufferPosition = vBuffer.position()
+    val uBufferLimit = uBuffer.limit()
+
+    // Advance the V buffer by 1 byte, since the U buffer will not contain the first V value.
+    vBuffer.position(vBufferPosition + 1)
+    // Chop off the last byte of the U buffer, since the V buffer will not contain the last U value.
+    uBuffer.limit(uBufferLimit - 1)
+
+    // Check that the buffers are equal and have the expected number of elements.
+    val areNV21 =
+        (vBuffer.remaining() == (2 * imageSize / 4 - 2)) && (vBuffer.compareTo(uBuffer) == 0)
+
+    // Restore buffers to their initial state.
+    vBuffer.position(vBufferPosition)
+    uBuffer.limit(uBufferLimit)
+
+    return areNV21
+}
